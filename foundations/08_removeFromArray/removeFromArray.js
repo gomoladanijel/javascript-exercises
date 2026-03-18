612b0a874bb2005c91e3d54ee49689c2bf61b1fd
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-19 00:27:51 +0100

    Create function that removes item(s) from array
    Function removeFromArray() takes array as first
    argument and specific item(s) to be removed from
    array as other arguments.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(args) {
+    args = [...arguments];
+    const uniqArr = [...new Set(args[0])];
+
+    for (let i = 1; i < arguments.length; i++) {
+        if (uniqArr.includes(arguments[i])) {
+            uniqArr.splice(uniqArr.indexOf(arguments[i]), 1);
+        }
+    }
+
+    return uniqArr;
 };
 
 // Do not edit below this line
