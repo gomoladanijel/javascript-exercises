77c2398844f12e1ceede9cc3b87f7a13f44f8333
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-19 01:31:41 +0100

    Create function that sums integers
    Function sumAll() takes two integers as arguments
    and returns a sum of every integer between
    (and including) them.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    } else if (Number.isNaN(start) || Number.isNaN(end)) {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = start; i >= end; i--) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
