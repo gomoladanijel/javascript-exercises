1e93ae01df5bdf26c64127466efeb1d1c705de22
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-24 16:03:08 +0100

    Create different calculation functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	return numArr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(n, e) {
+	return n ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+
+	if (n === 0 || n === 1) {
+    fact;
+  } else {
+    for (let i = n; i >= 1; i--) {
+      fact *= i;
+    }
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
