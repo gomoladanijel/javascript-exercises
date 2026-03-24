8bf4ac8fcbc6bb526d538795a9a4be116f4dfeb0
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-24 17:38:32 +0100

    Create function that returns the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
 
+  return death - birth;
+};
+
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line
