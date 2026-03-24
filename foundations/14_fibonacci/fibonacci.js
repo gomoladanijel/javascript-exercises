bf02c0a7fbca94e3e0948e4aa928c00e39d3fe3a
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-24 17:08:42 +0100

    Create function that gives required Fibonnaci member

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
 
+    const fib = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[n];
 };
 
 // Do not edit below this line
